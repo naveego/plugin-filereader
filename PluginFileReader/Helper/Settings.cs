20f91edbde3ef88c65655cc36fd80da7a0fbce9c
Wyatt Roehler <wyatt.roehler@naveego.com>
2021-03-23 23:11:55 -0400

    fix(read): better query parsing for getting rootpaths

--- PluginFileReader/Helper/Settings.cs
+++ PluginFileReader/Helper/Settings.cs
@@ -266,8 +266,10 @@ namespace PluginFileReader.Helper
         /// <returns></returns>
         public List<RootPathObject> GetRootPathsFromQuery(string query)
         {
-            var fromSplits = query.ToLower().Split("from");
-            var joinSplits = query.ToLower().Split("join");
+            var noCommentsQuery = RemoveComments(query, true).Replace('\n', ' ');
+            
+            var fromSplits = noCommentsQuery.ToLower().Split("from");
+            var joinSplits = noCommentsQuery.ToLower().Split("join");
 
             Logger.Debug($"From splits {JsonConvert.SerializeObject(fromSplits, Formatting.Indented)}");
             Logger.Debug($"Join splits {JsonConvert.SerializeObject(joinSplits, Formatting.Indented)}");
@@ -283,7 +285,8 @@ namespace PluginFileReader.Helper
                 }
 
                 var tableName = selectTableSplit.TrimStart('[').TrimEnd(']');
-                rootPaths.AddRange(RootPaths.FindAll(r => tableName.Contains(new DirectoryInfo(r.RootPathName()).Name.ToLower())));
+                rootPaths.AddRange(RootPaths.FindAll(r =>
+                    tableName.Contains(new DirectoryInfo(r.RootPathName()).Name.ToLower())));
             }
 
             foreach (var joinSplit in joinSplits.Skip(1))
@@ -296,10 +299,73 @@ namespace PluginFileReader.Helper
                 }
 
                 var tableName = joinTableSplit.TrimStart('[').TrimEnd(']');
-                rootPaths.AddRange(RootPaths.FindAll(r => tableName.Contains(new DirectoryInfo(r.RootPathName()).Name.ToLower())));
+                rootPaths.AddRange(RootPaths.FindAll(r =>
+                    tableName.Contains(new DirectoryInfo(r.RootPathName()).Name.ToLower())));
             }
 
             return rootPaths.Where(r => r != null).GroupBy(r => r.RootPathName()).Select(g => g.First()).ToList();
+        }
+
+        private string RemoveComments(string input, bool preservePositions, bool removeLiterals = false)
+        {
+            var everythingExceptNewLines = new Regex("[^\r\n]");
+            
+            var lineComments = @"--(.*?)\r?\n";
+            var lineCommentsOnLastLine =
+                @"--(.*?)$"; // because it's possible that there's no \r\n after the last line comment
+            // literals ('literals'), bracketedIdentifiers ([object]) and quotedIdentifiers ("object"), they follow the same structure:
+            // there's the start character, any consecutive pairs of closing characters are considered part of the literal/identifier, and then comes the closing character
+            var literals = @"('(('')|[^'])*')"; // 'John', 'O''malley''s', etc
+            var bracketedIdentifiers = @"\[((\]\])|[^\]])* \]"; // [object], [ % object]] ], etc
+            var quotedIdentifiers =
+                @"(\""((\""\"")|[^""])*\"")"; // "object", "object[]", etc - when QUOTED_IDENTIFIER is set to ON, they are identifiers, else they are literals
+            //var blockComments = @"/\*(.*?)\*/";  //the original code was for C#, but Microsoft SQL allows a nested block comments // //https://msdn.microsoft.com/en-us/library/ms178623.aspx
+            //so we should use balancing groups // http://weblogs.asp.net/whaggard/377025
+            var nestedBlockComments = @"/\*
+                                (?>
+                                /\*  (?<LEVEL>)      # On opening push level
+                                | 
+                                \*/ (?<-LEVEL>)     # On closing pop level
+                                |
+                                (?! /\* | \*/ ) . # Match any char unless the opening and closing strings   
+                                )+                         # /* or */ in the lookahead string
+                                (?(LEVEL)(?!))             # If level exists then fail
+                                \*/";
+
+            var noComments = Regex.Replace(input,
+                nestedBlockComments + "|" + lineComments + "|" + lineCommentsOnLastLine + "|" + literals + "|" +
+                bracketedIdentifiers + "|" + quotedIdentifiers,
+                me =>
+                {
+                    if (me.Value.StartsWith("/*") && preservePositions)
+                        return
+                            everythingExceptNewLines.Replace(me.Value,
+                                " "); // preserve positions and keep line-breaks // return new string(' ', me.Value.Length);
+                    else if (me.Value.StartsWith("/*") && !preservePositions)
+                        return "";
+                    else if (me.Value.StartsWith("--") && preservePositions)
+                        return everythingExceptNewLines.Replace(me.Value,
+                            " "); // preserve positions and keep line-breaks
+                    else if (me.Value.StartsWith("--") && !preservePositions)
+                        return everythingExceptNewLines.Replace(me.Value,
+                            ""); // preserve only line-breaks // Environment.NewLine;
+                    else if (me.Value.StartsWith("[") || me.Value.StartsWith("\""))
+                        return me.Value; // do not remove object identifiers ever
+                    else if (!removeLiterals) // Keep the literal strings
+                        return me.Value;
+                    else if (removeLiterals && preservePositions
+                    ) // remove literals, but preserving positions and line-breaks
+                    {
+                        var literalWithLineBreaks = everythingExceptNewLines.Replace(me.Value, " ");
+                        return "'" + literalWithLineBreaks.Substring(1, literalWithLineBreaks.Length - 2) + "'";
+                    }
+                    else if (removeLiterals && !preservePositions) // wrap completely all literals
+                        return "''";
+                    else
+                        throw new NotImplementedException();
+                },
+                RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace);
+            return noComments;
         }
 
         /// <summary>
@@ -629,7 +695,8 @@ namespace PluginFileReader.Helper
                                     {
                                         try
                                         {
-                                            var fileStream = Utility.GetStream(localTestFileName, Constants.FileModeLocal, true);
+                                            var fileStream = Utility.GetStream(localTestFileName,
+                                                Constants.FileModeLocal, true);
                                             client.UploadFile(fileStream.Stream, remoteTestFileName);
                                             fileStream.Close();
                                             Utility.DeleteFileAtPath(localTestFileName, rootPath, this, true);
